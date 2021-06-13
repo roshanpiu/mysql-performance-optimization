7403169a7439fe882d8e2ba53fd4a60eab32e5c0
Roshan Piumal <rpiumal@mitrai.com>
2021-06-14 00:19:27 +0530

    best practiceses added

--- 12-NEQ-Index.sql
+++ 12-NEQ-Index.sql
@@ -19,7 +19,10 @@ EXPLAIN SELECT *
 FROM customer
 WHERE store_id = 1;
 
-
+-- use explain to check the execution plan
+-- avoid using <> operator as it igonores the index
+-- use union all if you don't care about duplicate data
+-- always test the QUERY on your development server with test data before promoting to production
 
 
 
